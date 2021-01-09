6ef3f66a5ec1a8e028dedcb77da9fc002eac07a4
Christopher Odom <christopher.r.odom@gmail.com>
2021-01-09 11:55:56 -0500

    Added in baseline validation layer configuration
    The program now checks for validation layer
    support (if requested via preprocessor checks)
    and sets up the validation layer plugin to
    use the debugCallback(...) function defined
    on line 73.
    
    This is done by using the vkCreateDebugUtilsMessengerEXT
    function, which must be loaded through the
    vkGetInstanceProcAddr function since vulkan extension
    functions aren't exported in the global namespace.
    This vkCreateDebugUtilsMessengerEXT function takes
    in a VkDebugUtilsMessengerCreateInfoEXT struct
    which configures the debug levels and the callback
    function, and allocates
    a messenger object for the current instance.
    
    This same structure is also passed into the
    pNext attribute in the VkInstaceCreateInfo struct
    so that the same callback can be used upon
    instance creation and destruction.
    
    There are a few helper functions that wrap
    all of the tedious and ugly struct population
    and dynamic function loading so that the code
    is a bit readable, but it's still a bit of a
    mess.
    
    The next tutorial will have the program select
    physical devices.

--- triangle/main.cc
+++ triangle/main.cc
@@ -3,16 +3,122 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
+#include <cstring>
 #include <cstdlib>
 
 const uint32_t WIDTH = 1280;
 const uint32_t HEIGHT = 720;
 
+/* Globals to enable vulkan's valication layer */
+const std::vector<const char*> validationLayers =
+{
+	"VK_LAYER_KHRONOS_validation"
+};
 
-class HelloTriangleApplication {
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+
+/* Query the instance layer properties for validation layer support */
+bool checkValidationLayerSupport()
+{
+	bool layerFound = false;
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr); // Will set layerCount to the count
+	
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for (const char* layerName: validationLayers)
+	{
+
+		for (const auto& layerProps: availableLayers)
+		{
+			if(strcmp(layerName, layerProps.layerName) == 0)
+			{
+				layerFound = true;
+				break;
+			}
+		}
+	}
+	return layerFound;
+}
+
+/* Function will generate an extension list
+ * conditionally based off of whether validation layers
+ * are enabled or not */
+std::vector<const char*> getRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions+glfwExtensionCount);
+
+	if (enableValidationLayers)
+	{
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+
+	return extensions;
+}
+
+/* generic debug callback function that
+ * hooks into the validation layers
+ */
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData)
+{
+	std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+	return VK_FALSE;
+}
+
+/* Load the vkCreateDebugUtilsMessengerEXT function
+ * from it's extension, and then execute it */
+VkResult CreateDebugUtilsMessengerEXT(
+		VkInstance instance,
+		const VkDebugUtilsMessengerCreateInfoEXT* pcreateInfo,
+		const VkAllocationCallbacks* pAllocator,
+		VkDebugUtilsMessengerEXT* pDebugMessenger)
+{
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+
+	if(func != nullptr)
+	{
+		return func(instance, pcreateInfo, pAllocator, pDebugMessenger);
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void DestroyDebugUtilsMessengerEXT(
+		VkInstance instance,
+		VkDebugUtilsMessengerEXT DebugMessenger,
+		const VkAllocationCallbacks* pAllocator)
+{
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+
+	if(func != nullptr)
+	{
+		func(instance, DebugMessenger, pAllocator);
+	}
+}
+
+class HelloTriangleApplication
+{
 public:
-	void run() {
+	void run()
+	{
 		initWindow();
 		initVulkan();
 		mainLoop();
@@ -21,6 +127,7 @@ public:
 
 private:
 	VkInstance instance;
+	VkDebugUtilsMessengerEXT debugMessenger;
 	GLFWwindow* window;
 
 	void initWindow()
@@ -34,10 +141,16 @@ private:
 	void initVulkan()
 	{
 		createInstance(); // Internal function to handle vulkan bookend
+		setupDebugMessenger();
 	}
 
 	void createInstance()
 	{
+		if (enableValidationLayers && !checkValidationLayerSupport())
+		{
+			throw std::runtime_error("validation layers requested, but not available!");
+		}
+
 		/* Vulkan takes in structs as arguments instead of arg lists,
 		 * this first struct defines application data */
 		VkApplicationInfo appInfo{};
@@ -53,15 +166,24 @@ private:
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
 
-		/* GLFW provides handy extensions for vulkan, so we query for them and use them */
-		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions;
+		auto extensions = getRequiredExtensions();
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+		createInfo.ppEnabledExtensionNames = extensions.data();
 
-		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		/* Setup instance constructor/destructor debugging */
+		VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
+		if(enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
 
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
-		createInfo.enabledLayerCount = 0;
+			populateDebugMessengerCreateInfo(debugCreateInfo);
+			createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
+		} else {
+			createInfo.enabledLayerCount = 0;
+
+			createInfo.pNext = nullptr;
+		}
 
 		/* Finally the vulkan instance creation */
 		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
@@ -69,6 +191,41 @@ private:
 		{
 			throw std::runtime_error("failed to create vk instance!");
 		}
+	}
+
+	/* Tells vulkan validation layers where and when
+	 * to use the callback function defined earlier in
+	 * this file */
+	void setupDebugMessenger()
+	{
+		VkDebugUtilsMessengerCreateInfoEXT createInfo;
+		if(!enableValidationLayers) return;
+
+		populateDebugMessengerCreateInfo(createInfo);
+
+		if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to setup debug messenger!");
+		}
+
+	}
+
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
+	{
+		/* Struct contains callback and messenger details */
+		createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		createInfo.messageSeverity =
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType =
+			VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		createInfo.pfnUserCallback = debugCallback;
+		createInfo.pUserData = nullptr;
+
 	}
 
 	void mainLoop()
@@ -82,6 +239,11 @@ private:
 
 	void cleanup()
 	{
+		if(enableValidationLayers)
+		{
+			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+		}
+
 		vkDestroyInstance(instance, nullptr);
 
 		glfwDestroyWindow(window);
